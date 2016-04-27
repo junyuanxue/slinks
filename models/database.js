dfdd96234e63283875d2b638c62c8c67cf2e3dd2
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-27 19:04:06 +0100

    took out not null from url text

--- models/database.js
+++ models/database.js
@@ -3,5 +3,5 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost/slinks'
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE slinks(id SERIAL PRIMARY KEY, url TEXT not null, starred BOOLEAN)');
+var query = client.query('CREATE TABLE slinks(id SERIAL PRIMARY KEY, url TEXT, starred BOOLEAN)');
 query.on('end', function() { client.end(); });
